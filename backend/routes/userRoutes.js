1e7840296cc4c87514773bdf07659dad3c547f7a
Sanskriti Lamichhane <lamichhanesans@gmail.com>
2024-08-08 14:18:24 +0100

    Updated code inside the routes folder.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
+import {signupUser} from '../controllers/userController.js';
 
 const router = express.Router();
 
-router.get("/signup",signupUser);
+router.post("/signup",signupUser);
 
 export default router;

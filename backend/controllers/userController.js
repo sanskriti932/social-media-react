fbf556dd807796ea5762a005490441cb7ed017d8
Sanskriti Lamichhane <lamichhanesans@gmail.com>
2024-08-09 14:14:42 +0100

    Added routes for user login,signup and user logout, user follow/unfollow with its logic in userController.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,38 +1,86 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js";
 
-const signupUser=async(req,res) =>{
-    try {
-        const {name,email,username,password}=req.body;
-        const user = await User.findOne({$or:[{email},{username}]});
-        if(user){
-            return res.status(400).json({message:"User already exists"});
-
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword= await bcrypt.hash(password,salt);
-        const newUser=new User({
-            name,
-            email,
-            username,
-            password:hashedPassword,
-        })
-        await newUser.save();
-        if(newUser){
-            res.status(201).json({
-                _id:newUser._id,
-                name:newUser.name,
-                email:newUser.email,
-                username:newUser.username,
-            })
-        }else{
-            res.status(400).json({message:"Invalid user data"});
-        }
-    } catch (err) {
-        res.status(500).json({message:err.message})
-        console.log("Error in signupUser: ",err.message)
+const signupUser = async (req, res) => {
+  try {
+    const { name, email, username, password } = req.body;
+    const user = await User.findOne({ $or: [{ email }, { username }] });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
     }
-}
 
-export {signupUser};
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in signupUser: ", err.message);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid username or password" });
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in loginUser: ", err.message);
+  }
+};
+
+const logoutUser = (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in loginUser: ", err.message);
+  }
+};
+
+const followUnFollowUser = async (req, res) => {
+  try {
+    const {id} = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log("Error in loginUser: ", err.message);
+  }
+};
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser };
